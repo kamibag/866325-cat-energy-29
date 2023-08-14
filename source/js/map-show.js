81d55d8e07217e39d64fc599bdae91021e5795b5
kamibag <kamilayk@mail.ru>
2023-08-14 18:23:27 +0300

    добавлен скрипт меню и исправлена главная

---
+++ source/js/map-show.js
@@ -0,0 +1,2 @@
+document.querySelector('.contacts__map-picture').classList.remove('location__map-picture--nojs');
+document.querySelector('.contacts__map-iframe').classList.remove('location__map-iframe--nojs');
