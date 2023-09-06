ba9b6a8b9cb76ac67a67c95efa62ce9556ba5b08
kamibag <kamilayk@mail.ru>
2023-09-06 11:08:53 +0300

    карта в виде картинки добавлена в noscript

--- source/js/map-show.js
+++ source/js/map-show.js
@@ -1,2 +1,1 @@
-document.querySelector('.contacts__map-picture').classList.remove('contacts__map-picture--no-js');
 document.querySelector('.contacts__map-iframe').classList.remove('contacts__map-iframe--no-js');
