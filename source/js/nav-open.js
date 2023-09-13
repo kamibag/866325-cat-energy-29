8daa94e51a086ffdc212b7efa6970ba560902645
kamibag <kamilayk@mail.ru>
2023-09-13 19:48:56 +0300

    исправлены кавычки в js

--- source/js/nav-open.js
+++ source/js/nav-open.js
@@ -7,10 +7,10 @@ navButton.addEventListener('click', function () {
   if (navMain.classList.contains('nav--closed')) {
     navMain.classList.remove('nav--closed');
     navMain.classList.add('nav--opened');
-    navButton.setAttribute("aria-label", "Закрыть меню");
+    navButton.setAttribute('aria-label', 'Закрыть меню');
   } else {
     navMain.classList.add('nav--closed');
     navMain.classList.remove('nav--opened');
-    navButton.setAttribute("aria-label", "Открыть меню");
+    navButton.setAttribute('aria-label', 'Открыть меню');
   }
 });
